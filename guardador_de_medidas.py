045e4b00915cdda7d806e9abffdd62b0572002bf
Íñigo <inigo.casanova+git@gmail.com>
2025-12-06 14:16:06 -0600

    añadir algunos documentos faltantes

---
+++ guardador_de_medidas.py
@@ -0,0 +1,48 @@
+from serial.tools import list_ports
+import serial
+import time
+import json
+
+archivo_logs = "logs.json"
+
+#Estas línas sirven para identificar manualmente el puerto serial del arduino.
+ports = list_ports.comports()
+for port in ports: print(port)
+
+
+#Esta línea se conecta al puerto serial. Seguramente hay que cambiarla dependiendo de la computadora.
+serialCom = serial.Serial('/dev/cu.usbmodemFA121', 57600)
+
+#Resetear el arduino
+serialCom.setDTR(False)
+time.sleep(1)
+serialCom.flushInput()
+serialCom.setDTR(True)
+
+#Abrimos el archivo de los logs.
+with open(archivo_logs, 'r') as file:
+	eventosJSON = json.load(file)
+
+#Cargamos los eventos que ya tenemos
+eventos = eventosJSON['eventos']
+
+
+#Definimos las llaves para el diccionario que serán los eventos.
+llaves = ["tipo", "fecha", "hora"]
+
+
+while(1):
+	try:
+		#Leemos una linea del serial y la decodificamoes
+		s_bytes = serialCom.readline()
+		decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
+		datos = decoded_bytes.split(',')
+		nuevo_evento = dict(zip(llaves, datos))
+		eventos.append(nuevo_evento)
+		print("Nuevo evento registrado")
+	except:
+		print("Error: no se pudo leer una línea")
+	#Actualizamos la lista de eventos y el archivo
+	nuevos_eventos = json.dumps(dict(eventos = eventos), indent=4)
+	with open(archivo_logs, 'w') as file:
+		file.write(nuevos_eventos)
