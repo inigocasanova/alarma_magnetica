447db8e77d08a06487dd7ed9cc0fb6c217c6ce2d
inigocasanova <inigo.casanova@ciencias.unam.mx>
2025-12-06 13:00:07 -0600

    re-acomodo 1

---
+++ buscar_datos.js
@@ -0,0 +1,29 @@
+	//Creo que buscamos el elemento donde vamos a poner la tabla.
+	const listaDeActividad = document.querySelector("#lista-actividad");
+
+	//Función para generar la tabla.
+	const generarTabla = (datos) => {
+		//Iteramos en los eventos
+		datos.eventos.forEach((evento) => {
+			//Creamos unos divs
+			const nuevoDivDeLinea = document.createElement('tr');
+
+			//Definimos algo de html que ponerle a nuestro nuevo div.
+			const movimientoHTML = `
+			<td>${evento.tipo}</td>
+			<td>${evento.fecha}</td>
+			<td>${evento.hora}</td>
+			`;
+		
+			//Le metemos el html definido a listaDeActividad.
+			nuevoDivDeLinea.innerHTML = movimientoHTML;
+			//Agregar el div al div de las listas.
+			listaDeActividad.appendChild(nuevoDivDeLinea);
+		});
+	};
+
+		
+	//Agarramos los datos del documento.
+	fetch("../logs.json")
+	.then(response => response.json())
+	.then(datos => generarTabla(datos));
